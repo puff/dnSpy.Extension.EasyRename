9688d49f0e665e69e6f9554fd86aec478dbb6ec3
puff <33184334+puff@users.noreply.github.com>
2023-05-21 18:25:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+dnSpy.Extension.EasyRename
+==========================
+
+A simple [dnSpyEx](https://github.com/dnSpyEx/dnSpy) extension for easily renaming members.
+
+## Features
+- [ ] Rename overridden methods automatically.
+- [ ] Rename type when renaming constructor method.
---
+++ dnSpy.Extension.EasyRename/Extension.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using dnSpy.Contracts.Extension;
+
+namespace EasyRename;
+
+[ExportExtension]
+internal sealed class Extension : IExtension
+{
+    public IEnumerable<string> MergedResourceDictionaries
+    {
+        // We don't have any extra resource dictionaries
+        get { yield break; }
+    }
+
+    public ExtensionInfo ExtensionInfo => new()
+    {
+        ShortDescription = "A simple extension for easily renaming members"
+    };
+
+    public void OnEvent(ExtensionEvent @event, object? obj)
+    {
+        // We don't care about any events
+    }
+}
